ce9cd8361778b34e1da5ec0201c2df42f053e610
Daniel Perez Alvarez <unindented@gmail.com>
2015-10-13 16:51:56 +0100

    Allow polymorphic arrays

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,15 +1,28 @@
 import isObject from 'lodash/lang/isObject';
 
 export default class ArraySchema {
-  constructor(itemSchema) {
+  constructor(itemSchema, options = {}) {
     if (!isObject(itemSchema)) {
       throw new Error('ArraySchema requires item schema to be an object.');
     }
 
     this._itemSchema = itemSchema;
+
+    if (options.schemaAttribute) {
+      const schemaAttribute = options.schemaAttribute;
+      this._getSchema = typeof schemaAttribute === 'function' ? schemaAttribute : x => x[schemaAttribute];
+    }
   }
 
   getItemSchema() {
     return this._itemSchema;
+  }
+
+  isPolymorphicSchema() {
+    return !!this._getSchema;
+  }
+
+  getSchemaKey(item) {
+    return this._getSchema(item);
   }
 }
