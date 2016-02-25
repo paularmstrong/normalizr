5d5e5e17f8fc1f153d7c326230b0a2fd385b7f59
Paul Armstrong <paul@spaceyak.com>
2016-02-25 11:17:54 -0800

    Give assignEntity access to the original input object
    Resolves gh-64

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function visitObject(obj, schema, bag, options) {
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
       const entity = visit(obj[key], schema[key], bag, options);
-      assignEntity.call(null, normalized, key, entity);
+      assignEntity.call(null, normalized, key, entity, obj);
     }
   }
   return normalized;
