ec22af3a8dbea778d61b88a72b50df81e4e2dd91
Daniel Perez Alvarez <unindented@gmail.com>
2016-04-06 09:14:22 +0100

    Add type declarations
    closes gh-84

---
+++ test/typescript/normalize.ts
@@ -0,0 +1,45 @@
+import {Schema, normalize, arrayOf, unionOf, valuesOf} from "../../index.d.ts";
+
+const isObject = function (obj: any) {
+  return typeof obj === 'object';
+};
+
+const assignEntity = function (output: any, key: string, value: any) {
+  if (key === "timestamp") {
+    output[key] = Date.now()
+  }
+  output[key] = value;
+};
+
+const mergeIntoEntity = function (entityA: any, entityB: any, entityKey: string) {
+  for (const key in entityB) {
+    if (!entityB.hasOwnProperty(key)) {
+      continue;
+    }
+
+    if (!entityA.hasOwnProperty(key)) {
+      entityA[key] = entityB[key];
+      continue;
+    }
+
+    if (isObject(entityA[key]) && isObject(entityB[key])) {
+      // Merge the two entities.
+      continue;
+    }
+  }
+};
+
+const user = new Schema("users");
+const group = new Schema("groups", { assignEntity });
+const member = unionOf({ users: user, groups: group }, { schemaAttribute: "type" });
+
+group.define({
+  members: arrayOf(member),
+  owner: member,
+  relations: valuesOf(member)
+});
+
+const normalizeWithObject = normalize({}, { group });
+const normalizeWithArray = normalize([], { groups: arrayOf(group) });
+const normalizeWithAssignEntity = normalize([], { groups: arrayOf(group) }, { assignEntity });
+const normalizeWithMergeIntoEntity = normalize([], { groups: arrayOf(group) }, { mergeIntoEntity });
---
+++ test/typescript/schema.ts
@@ -0,0 +1,38 @@
+import {Schema, IterableSchema, UnionSchema, SchemaValue} from "../../index.d.ts";
+
+const idAttribute = "slug";
+const meta = { removeProps: ["year", "publisher"] };
+
+const generateSlug = function (entity: any) {
+  if (entity.slug != null) {
+    return "slug";
+  }
+  return "id";
+};
+
+const assignEntity = function (output: any, key: string, value: any, input: any, schema: SchemaValue) {
+  const itemSchema = (schema instanceof IterableSchema || schema instanceof UnionSchema) ?
+    schema.getItemSchema() : schema;
+  const removeProps = (itemSchema instanceof Schema) ?
+    itemSchema.getMeta("removeProps") : null;
+  if (!removeProps || removeProps.indexOf(key) < 0) {
+    output[key] = value;
+  }
+};
+
+const schemaWithKey = new Schema("articles");
+const schemaWithStringIdAttribute = new Schema("articles", { idAttribute });
+const schemaWithFunctionIdAttribute = new Schema("articles", { idAttribute: generateSlug });
+const schemaWithMeta = new Schema("articles", { meta });
+const schemaWithAssignEntity = new Schema("articles", { meta, assignEntity });
+
+const someKey: string = schemaWithKey.getKey();
+const someIdAttribute: string = schemaWithStringIdAttribute.getIdAttribute();
+const someOtherIdAttribute: string = schemaWithFunctionIdAttribute.getIdAttribute();
+const someMeta: any = schemaWithMeta.getMeta("removeProps");
+
+const article = new Schema("articles");
+const user = new Schema("users");
+article.define({
+  author: user
+});
