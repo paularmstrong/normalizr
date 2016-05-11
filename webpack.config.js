ab4cc251c6b4f95bbe2e4c0a81b87b58bf34dd5a
Paul Armstrong <paul@spaceyak.com>
2016-05-11 09:41:01 -0700

    Update babel, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index',
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
-        ]
-    },
-    output: {
-        filename: 'dist/normalizr.min.js',
-        libraryTarget: 'umd',
-        library: 'normalizr'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
+  entry: './src/index',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-1']
+        }
+      }
     ]
+  },
+  output: {
+    filename: 'dist/normalizr.min.js',
+    libraryTarget: 'umd',
+    library: 'normalizr'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
 };
