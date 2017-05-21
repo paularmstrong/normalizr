bdcefef63fcf5544da23763d5e3ef77ae902e708
Nicolas <nicolas.gasp@free.fr>
2017-05-21 16:31:31 +0200

    added function type check

--- src/schemas/ImmutableUtils.js
+++ src/schemas/ImmutableUtils.js
@@ -12,8 +12,8 @@
  */
 export function isImmutable(object) {
   return !!(object && (
-    (object.hasOwnProperty && object.hasOwnProperty('__ownerID')) || // Immutable.Map
-    (object._map && object._map.hasOwnProperty && object._map.hasOwnProperty('__ownerID')) // Immutable.Record
+    (typeof object.hasOwnProperty === 'function' && object.hasOwnProperty('__ownerID')) || // Immutable.Map
+    (object._map && typeof object._map.hasOwnProperty === 'function' && object._map.hasOwnProperty('__ownerID')) // Immutable.Record
   ));
 }
 
