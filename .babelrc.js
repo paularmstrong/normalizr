ed279c26b569742a563673a923b4bcb58546d07b
Paul Armstrong <paul@spaceyak.com>
2018-04-17 15:15:07 -0700

    updates to eslint for flow

--- .babelrc.js
+++ .babelrc.js
@@ -5,10 +5,10 @@ const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test';
 module.exports = {
   presets: [['env', { modules: false, loose: true }]],
   plugins: [
+    'transform-flow-strip-types'
     cjs && 'transform-es2015-modules-commonjs',
     'transform-object-rest-spread',
     // TODO: use 'loose' mode for this after upgrading to babel@7
-    'transform-class-properties',
-    'transform-flow-strip-types'
+    'transform-class-properties'
   ].filter(Boolean)
 };
