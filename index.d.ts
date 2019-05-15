2380aacfbe6a7ca87518e84ab2b74af92d387dc0
= <=>
2019-05-15 20:06:35 +0200

    Fixed error

--- index.d.ts
+++ index.d.ts
@@ -3,65 +3,58 @@ declare namespace schema {
   export type SchemaFunction = (value: any, parent: any, key: string) => string;
   export type MergeFunction = (entityA: any, entityB: any) => any;
 
-  export class Array extends BaseSchema {
-    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction)
-    define(definition: Schema): void
+  export class Array implements BaseSchema {
+    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction);
+    define(definition: Schema): void;
   }
 
   export interface EntityOptions {
-    idAttribute?: string | SchemaFunction
-    mergeStrategy?: MergeFunction
-    processStrategy?: StrategyFunction
+    idAttribute?: string | SchemaFunction;
+    mergeStrategy?: MergeFunction;
+    processStrategy?: StrategyFunction;
   }
 
   export interface BaseSchema {
     schema: Schema;
   }
 
-  export class Entity extends BaseSchema {
-    constructor(key: string, definition?: Schema, options?: EntityOptions)
-    define(definition: Schema): void
-    key: string
+  export class Entity implements BaseSchema {
+    constructor(key: string, definition?: Schema, options?: EntityOptions);
+    define(definition: Schema): void;
+    key: string;
   }
 
-  export class Object extends BaseSchema {
-    constructor(definition: {[key: string]: Schema})
-    define(definition: Schema): void
+  export class Object implements BaseSchema {
+    constructor(definition: { [key: string]: Schema });
+    define(definition: Schema): void;
   }
 
-  export class Union extends BaseSchema {
-    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction)
-    define(definition: Schema): void
+  export class Union implements BaseSchema {
+    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction);
+    define(definition: Schema): void;
   }
 
-  export class Values extends BaseSchema {
-    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction)
-    define(definition: Schema): void
+  export class Values implements BaseSchema {
+    constructor(definition: Schema, schemaAttribute?: string | SchemaFunction);
+    define(definition: Schema): void;
   }
 }
 
 export type Schema =
-  schema.Array |
-  schema.Entity |
-  schema.Object |
-  schema.Union |
-  schema.Values |
-  schema.Array[] |
-  schema.Entity[] |
-  schema.Object[] |
-  schema.Union[] |
-  schema.Values[] |
-  {[key: string]: Schema | Schema[]};
+  | schema.Array
+  | schema.Entity
+  | schema.Object
+  | schema.Union
+  | schema.Values
+  | schema.Array[]
+  | schema.Entity[]
+  | schema.Object[]
+  | schema.Union[]
+  | schema.Values[]
+  | { [key: string]: Schema | Schema[] };
 
-export type NormalizedSchema<E, R> = { entities: E, result: R };
+export type NormalizedSchema<E, R> = { entities: E; result: R };
 
-export function normalize<E = any, R = any>(
-  data: any,
-  schema: Schema
-): NormalizedSchema<E, R>;
+export function normalize<E = any, R = any>(data: any, schema: Schema): NormalizedSchema<E, R>;
 
-export function denormalize(
-  input: any,
-  schema: Schema,
-  entities: any
-): any;
+export function denormalize(input: any, schema: Schema, entities: any): any;
