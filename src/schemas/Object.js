28b05b58d08b5d7f15204f37740a927071525460
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-27 19:17:17 +0100

    Add ability to dynamically set nested schema type

--- src/schemas/Object.js
+++ src/schemas/Object.js
@@ -4,7 +4,8 @@ export const normalize = (schema, input, parent, key, visit, addEntity, visitedE
   const object = { ...input };
   Object.keys(schema).forEach((key) => {
     const localSchema = schema[key];
-    const value = visit(input[key], input, key, localSchema, addEntity, visitedEntities);
+    const resolvedLocalSchema = typeof localSchema === 'function' ? localSchema(input) : localSchema;
+    const value = visit(input[key], input, key, resolvedLocalSchema, addEntity, visitedEntities);
     if (value === undefined || value === null) {
       delete object[key];
     } else {
