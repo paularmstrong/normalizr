3067a27dfec44f7a21d726571036c508539885c7
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-28 14:56:46 +0100

    Use blocks

--- typescript-tests/object.ts
+++ typescript-tests/object.ts
@@ -5,8 +5,12 @@ const data = {
 };
 const user = new schema.Entity('users');
 
-const responseSchema = new schema.Object({ users: new schema.Array(user) });
-const normalizedData = normalize(data, responseSchema);
+{
+  const responseSchema = new schema.Object({ users: new schema.Array(user) });
+  const normalizedData = normalize(data, responseSchema);
+}
 
-const responseSchemaAlt = { users: new schema.Array(user) };
-const normalizedDataAlt = normalize(data, responseSchemaAlt);
+{
+  const responseSchema = { users: new schema.Array(user) };
+  const normalizedData = normalize(data, responseSchema);
+}
