8a2eaaa55e237af2828a09c29f876b142de1de1e
wurstbonbon <3285994+wurstbonbon@users.noreply.github.com>
2020-01-11 23:38:10 +0100

    Enable loose transformation for object spread operator to improve performance

--- .babelrc.js
+++ .babelrc.js
@@ -6,7 +6,7 @@ module.exports = {
   presets: [['@babel/preset-env', { loose: true }]],
   plugins: [
     // cjs && 'transform-es2015-modules-commonjs',
-    '@babel/plugin-proposal-object-rest-spread',
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }]
   ].filter(Boolean)
 };
